5b467a24b95af829736a0e52e1287ad1c0abf5c3
rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>
2026-06-17 19:29:03 +0700

    feat: modul users

---
+++ src/modules/users/dto/users.dto.ts
@@ -0,0 +1,42 @@
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from "class-validator";
+import { Role } from "../../../../generated/prisma/enums.js";
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty({ message: "Name is required" })
+  name!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: "Username is required" })
+  username!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: "Password is required" })
+  @MinLength(8, { message: "Password must be at least 8 characters" })
+  password!: string;
+
+  @IsNotEmpty({ message: "Role is required" })
+  @IsEnum(Role, { message: "Role must be ADMIN or CASHIER" })
+  role!: Role;
+}
+
+export class UpdateUserDTO {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8, { message: "Password must be at least 8 characters" })
+  password?: string;
+
+  @IsOptional()
+  @IsEnum(Role, { message: "Role must be ADMIN or CASHIER" })
+  role?: Role;
+}
