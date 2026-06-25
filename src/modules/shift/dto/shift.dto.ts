5a4262616b4cc7027d27c35f35f2a907f2356d6c
rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>
2026-06-25 16:10:57 +0700

    fix: shift.dto.ts untuk bug initialCash

--- src/modules/shift/dto/shift.dto.ts
+++ src/modules/shift/dto/shift.dto.ts
@@ -1,9 +1,8 @@
-import {
-  IsNumber,
-  Min,
-} from "class-validator";
+import { IsNumber, Min } from "class-validator";
+import { Type } from "class-transformer";
 
 export class OpenShiftDTO {
+  @Type(() => Number)
   @IsNumber()
   @Min(1000)
   initialCash!: number;
