38be3774b97dbf5c74f1fae2b8de06f8a69f5f02
rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>
2026-06-25 20:11:14 +0700

    fix: implementasi search dan pagination diserver side di modul transaction

--- src/modules/transactions/dto/transaction.dto.ts
+++ src/modules/transactions/dto/transaction.dto.ts
@@ -1,17 +1,5 @@
 import { Type } from "class-transformer";
-import {
-  ArrayMinSize,
-  IsArray,
-  IsEnum,
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  Min,
-  ValidateIf,
-  ValidateNested,
-} from "class-validator";
+import { ArrayMinSize, IsArray, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Min, Max, ValidateIf, ValidateNested } from "class-validator";
 import { PaymentMethod } from "../../../../generated/prisma/enums.js";
 
 export class CreateTransactionItemDTO {
@@ -49,4 +37,25 @@ export class FindTransactionQueryDTO {
   @IsOptional()
   @IsString()
   shiftId?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1, { message: "Page minimal 1" })
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100, { message: "Limit maksimal 100 data per request" })
+  limit: number = 10;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsString()
+  date?: string; // Format YYYY-MM-DD
 }
