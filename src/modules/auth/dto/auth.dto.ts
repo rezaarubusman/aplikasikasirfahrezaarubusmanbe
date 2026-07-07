ec92f1513beeb0ba1fc592de84854f3d2a283090
Fahreza Aqsa Arubusman <fahreza.arubusman@gmail.com>
2026-07-07 20:52:09 +0700

    fix: perapihan auth.dto.ts (#27)
    Co-authored-by: rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -3,45 +3,23 @@ import { Role } from "../../../../generated/prisma/enums.js";
 
 export class RegisterDTO {
   @IsString()
-  @IsNotEmpty({
-    message: "Name is required",
-  })
-  name!: string;
+  @IsNotEmpty({ message: "Name is required" }) name!: string;
 
   @IsString()
-  @IsNotEmpty({
-    message: "Username is required",
-  })
-  username!: string;
+  @IsNotEmpty({ message: "Username is required" }) username!: string;
 
   @IsString()
-  @IsNotEmpty({
-    message: "Password is required",
-  })
-  @MinLength(8, {
-    message:
-      "Password must be at least 8 characters",
-  })
-  password!: string;
+  @IsNotEmpty({ message: "Password is required" })
+  @MinLength(8, { message: "Password must be at least 8 characters" }) password!: string;
 
   @IsOptional()
-  @IsEnum(Role, {
-    message:
-      "Role must be ADMIN or CASHIER",
-  })
-  role?: Role;
+  @IsEnum(Role, { message: "Role must be ADMIN or CASHIER" }) role?: Role;
 }
 
 export class LoginDTO {
   @IsString()
-  @IsNotEmpty({
-    message: "Username is required",
-  })
-  username!: string;
+  @IsNotEmpty({ message: "Username is required" }) username!: string;
 
   @IsString()
-  @IsNotEmpty({
-    message: "Password is required",
-  })
-  password!: string;
+  @IsNotEmpty({ message: "Password is required" }) password!: string;
 }
